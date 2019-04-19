63913eaae8df638631b2ea17f571b8fe3f86ac0d
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-19 18:17:49 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -4,4 +4,11 @@
 
 def roll
   # code goes here
+  # using rand range method found here: https://www.rubyguides.com/2015/03/ruby-random/
+  rand(1..6)
+
+#I wanted to try using arrays but couldn't
+# number = (1,2,3,4,5,6)
+# number.shuffle.first
+  
 end
